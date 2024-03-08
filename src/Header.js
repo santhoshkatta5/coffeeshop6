0f4d1c1b5c094da5fcb5991090b37b62b12505c4
santhosh katta <santhoshkatta@lyrostech.com>
2024-03-08 17:56:22 +0530

    commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='hh'>
+            <div><h1>SURENDHRA'S SOFT TEA</h1></div>
+            <marquee>WELCOME TO SURENDHRA'S SOFT TEA</marquee>
+        </div>
+    )
+
+}
+
+export default Header
